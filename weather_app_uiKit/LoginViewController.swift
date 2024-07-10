5f368a466f41f10d5666fef7f50460899cdd512a
Soumik Bhattacharyya <soumikb1240@gmail.com>
2024-07-10 11:35:07 +0530

    update: tab view

---
+++ weather_app_uiKit/LoginViewController.swift
@@ -0,0 +1,28 @@
+//
+//  LoginViewController.swift
+//  weather_app_uiKit
+//
+//  Created by Nuveda on 10/07/24.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    @IBOutlet weak var username: UITextField!
+    
+    @IBOutlet weak var password: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+      
+    }
+    
+//    weak var btnLogin: UIImageView!{
+//        
+//        let vc = self.storyboard?.instantiateViewController(withIdentifier: "LoginViewController") as! LoginViewController
+//    
+//        if username == "Soumik" && password == "soumik"{
+//            performSegue(withIdentifier: "showNextPage", sender: self)
+//        }
+//    }
+}
---
+++ weather_app_uiKit/LoginVm.swift
@@ -0,0 +1,8 @@
+//
+//  LoginVm.swift
+//  weather_app_uiKit
+//
+//  Created by Nuveda on 10/07/24.
+//
+
+import Foundation
